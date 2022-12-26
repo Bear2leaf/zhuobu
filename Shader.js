92ca8c507b087f06cf4c71ee9dcb75161f0ad46e
Bear2leaf <845708495@qq.com>
2022-12-26 12:00:22 +0800

    our lovely triangle.

---
+++ Shader.js
@@ -0,0 +1,53 @@
+import { gl } from "./global.js";
+export default class Shader {
+    constructor(vs, fs) {
+        const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+        const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+        if (!vertexShader || !fragmentShader) {
+            throw new Error("shader not created");
+        }
+        gl.shaderSource(vertexShader, vs);
+        gl.shaderSource(fragmentShader, fs);
+        gl.compileShader(vertexShader);
+        gl.compileShader(fragmentShader);
+        if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+            throw new Error(gl.getShaderInfoLog(vertexShader) || "vertex shader compile error");
+        }
+        if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+            throw new Error(gl.getShaderInfoLog(fragmentShader) || "fragment shader compile error");
+        }
+        const program = gl.createProgram();
+        if (!program) {
+            throw new Error("program not created");
+        }
+        gl.attachShader(program, vertexShader);
+        gl.attachShader(program, fragmentShader);
+        gl.linkProgram(program);
+        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+            throw new Error(gl.getProgramInfoLog(program) || "program link error");
+        }
+        this.program = program;
+    }
+    useAndGetProgram() {
+        gl.useProgram(this.program);
+        return this.program;
+    }
+}
+export class DemoShader extends Shader {
+    constructor() {
+        super(`#version 300 es 
+            layout (location = 0) in vec4 a_position; 
+             
+            void main() { 
+              gl_Position = a_position; 
+            }`, `#version 300 es 
+            precision highp float; 
+             
+            out vec4 color; 
+             
+            void main() { 
+              color = vec4(1.0); 
+            }`);
+    }
+}
+//# sourceMappingURL=Shader.js.map
--- game.ts
+++ game.ts
@@ -1,4 +1,20 @@
 import ready, { device, gl } from "./global.js";
+import { DemoShader } from "./Shader.js";
 
 
-ready(()=>{})
+ready(() => {
+  const shader = new DemoShader()
+  shader.useAndGetProgram();
+  const vao = gl.createVertexArray();
+  const vbo = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+  gl.bindVertexArray(vao);
+  gl.enableVertexAttribArray(0);
+  gl.vertexAttribPointer(0, 4, gl.FLOAT, false, 0, 0);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    -0.5, -0.5, 0, 1,
+    0.5, -0.5, 0, 1,
+    0, 0.5, 0, 1
+  ]), gl.STATIC_DRAW);
+  gl.drawArrays(gl.TRIANGLES, 0, 3)
+})
