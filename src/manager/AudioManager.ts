afb71293276354dbf5db0e06e406bbf3a60783d9
Bear2leaf <845708495@qq.com>
2023-07-27 19:29:04 +0800

    add test demo audio

--- src/manager/AudioManager.ts
+++ src/manager/AudioManager.ts
@@ -1,8 +1,14 @@
+import DemoAudio from "../audio/DemoAudio.js";
 import Manager from "./Manager.js";
 
 
 export default class AudioManager extends Manager<unknown> {
     addObjects(): void {
+        [
+            DemoAudio
+        ].forEach(ctor => {
+            this.add(ctor);
+        });
     }
     async load(): Promise<void> {
     }
