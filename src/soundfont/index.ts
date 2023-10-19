5ca1c72f3c9a93dd14a53b447589de1ee37a949e
parks-hordes <parks-hordes.0o@icloud.com>
2023-10-19 19:38:51 +0800

    reduce soundfont size

--- src/soundfont/index.ts
+++ src/soundfont/index.ts
@@ -1,12 +1,12 @@
 import { _tone_0000_GeneralUserGS_sf2_file } from "./0000_GeneralUserGS_sf2_file.js";
-import { _tone_0110_GeneralUserGS_sf2_file } from "./0110_GeneralUserGS_sf2_file.js";
+// import { _tone_0110_GeneralUserGS_sf2_file } from "./0110_GeneralUserGS_sf2_file.js";
 import { _tone_0460_GeneralUserGS_sf2_file } from "./0460_GeneralUserGS_sf2_file.js";
-import { _tone_0470_GeneralUserGS_sf2_file } from "./0470_GeneralUserGS_sf2_file.js";
-import { _tone_0480_GeneralUserGS_sf2_file } from "./0480_GeneralUserGS_sf2_file.js";
-import { _tone_0520_GeneralUserGS_sf2_file } from "./0520_GeneralUserGS_sf2_file.js";
-import { _tone_0560_GeneralUserGS_sf2_file } from "./0560_GeneralUserGS_sf2_file.js";
-import { _tone_0600_GeneralUserGS_sf2_file } from "./0600_GeneralUserGS_sf2_file.js";
-import { _tone_0700_GeneralUserGS_sf2_file } from "./0700_GeneralUserGS_sf2_file.js";
+// import { _tone_0470_GeneralUserGS_sf2_file } from "./0470_GeneralUserGS_sf2_file.js";
+// import { _tone_0480_GeneralUserGS_sf2_file } from "./0480_GeneralUserGS_sf2_file.js";
+// import { _tone_0520_GeneralUserGS_sf2_file } from "./0520_GeneralUserGS_sf2_file.js";
+// import { _tone_0560_GeneralUserGS_sf2_file } from "./0560_GeneralUserGS_sf2_file.js";
+// import { _tone_0600_GeneralUserGS_sf2_file } from "./0600_GeneralUserGS_sf2_file.js";
+// import { _tone_0700_GeneralUserGS_sf2_file } from "./0700_GeneralUserGS_sf2_file.js";
 import { _tone_0730_GeneralUserGS_sf2_file } from "./0730_GeneralUserGS_sf2_file.js";
 
 
@@ -14,13 +14,13 @@ import { _tone_0730_GeneralUserGS_sf2_file } from "./0730_GeneralUserGS_sf2_file
 
 
 export {
-    _tone_0110_GeneralUserGS_sf2_file,
-    _tone_0600_GeneralUserGS_sf2_file,
-    _tone_0480_GeneralUserGS_sf2_file,
-    _tone_0700_GeneralUserGS_sf2_file,
-    _tone_0560_GeneralUserGS_sf2_file,
-    _tone_0520_GeneralUserGS_sf2_file,
-    _tone_0470_GeneralUserGS_sf2_file,
+    // _tone_0110_GeneralUserGS_sf2_file,
+    // _tone_0600_GeneralUserGS_sf2_file,
+    // _tone_0480_GeneralUserGS_sf2_file,
+    // _tone_0700_GeneralUserGS_sf2_file,
+    // _tone_0560_GeneralUserGS_sf2_file,
+    // _tone_0520_GeneralUserGS_sf2_file,
+    // _tone_0470_GeneralUserGS_sf2_file,
     _tone_0000_GeneralUserGS_sf2_file,
     _tone_0460_GeneralUserGS_sf2_file,
     _tone_0730_GeneralUserGS_sf2_file,
